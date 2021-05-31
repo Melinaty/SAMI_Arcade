996c1bd8cc5e1ed26959b4c5d1526f23a47702f7
Maurici0ML <alanmauriciomoraleslopez@gmail.com>
2021-05-31 17:16:18 -0500

    FEAT: piano keys

--- dynamics/js/piano.js
+++ dynamics/js/piano.js
@@ -1,44 +1,70 @@
 //Variables del canvas
 let canvas = document.getElementById("piano");
 let ctx = canvas.getContext("2d");
-
-let fallo = false; //Variable que nos permite saber si fallamos y detener la animacion.
+let reintentar = $("#reintentar");
+reintentar.hide();
+let y; //Variable que controla como van bajando las teclas, se incializa una tecla antes de la que es.
+let vel;
+//Variable que nos permite saber si fallamos y detener la animacion.
+let fallo; 
 
 //AUDIO Y ARREGLO DE TECLAS RANDOM --------------------------------------------------------------------------------
 //Constantes de los audios.
-const CANCION = ["rondo", "wetH", "wetH"]; //Almacena el nombre de las canciones.
+const CANCION = ["wetH", "rondo", "bumblebee"]; //Almacena el nombre de las canciones.
 let song_rand = Math.floor(Math.random()*10) % 3; //Variable que elije una cancion al azar.
-let audios =  new Array(); //Arreglo que va a almacenar las rutas de los distintos audios de cada cancion.
-let num_audio = 0; //Variable de control que permite recorrer el arreglo con las rutas.
+let audios; //Arreglo que va a almacenar las rutas de los distintos audios de cada cancion.
+let num_audio; //Variable de control que permite recorrer el arreglo con las rutas.
 let audio; //Variable de objeto audio.
+let rand; //Variable que cambiara su valor de una forma random del 0 al 3.
 
-const TECLAS = [56, 58, 58] //Almacena la canctidad de teclas (archivos) que tiene cada cancion.
+const TECLAS = [58, 56, 48] //Almacena la canctidad de teclas (archivos) que tiene cada cancion.
 let tecla = {
     alto: canvas.height/4,
     ancho: canvas.width/4
 }
-let teclas_rand = new Array();  //Arreglo que almacena las pociciones random de las teclas.
-
-//For que recorre la longitud del numero de teclas de la cancion, y en el, se crea el arreglo con las rutas de los archivos, ademas de el orden random de las teclas.
-for(let i=1; i<=TECLAS[song_rand]; i++)
-{   
-    let rand = Math.round(Math.random()*3);
-    //Se guarda en el arreglo las rutas completas con cada audio.
-    audios.push('../statics/media/piano_songs/'+CANCION[song_rand]+'/'+CANCION[song_rand]+'('+i+').mp3');
-
-    //Crea las posiciones de las teclas de forma randomizada del 0 al 3.
-    teclas_rand.push(rand);
-}
+let teclas_rand;  //Arreglo que almacena las pociciones random de las teclas.
 
 //FUNCIONES -------------------------------------------------------------------------------------------------------
+function inicializar (){
+    audios= new Array();
+    teclas_rand= new Array();
+    num_audio=0;
+    vel=0;
+    y = - tecla.alto
+    fallo = false;
+    for(let i=1; i<=TECLAS[song_rand]; i++)
+    {  
+        rand = Math.round(Math.random()*3);
+        //Se guarda en el arreglo las rutas completas con cada audio.
+        audios.push('../statics/media/piano_songs/'+CANCION[song_rand]+'/'+CANCION[song_rand]+'('+i+').mp3');
 
+        //Crea las posiciones de las teclas de forma randomizada del 0 al 3.
+        teclas_rand.push(rand);
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    basePian();
+    dib_teclas(teclas_rand, y);
+    tabla();
+}
+
+//Funcion que permite desplegar el puntaje.
+function puntaje (aciertos, velocidad){
+    velocidad *= 100;
+    let puntos = $("#puntos");
+    let redondear = Math.round(velocidad+(aciertos*10));
+    puntos.text(redondear);
+}
+
+//Funcion que permite detener la animacion y reproduce un adio de fallo
 function fin(){
     fallo=true;
+    cancelAnimationFrame(animacion);
     audio = new Audio("../statics/media/piano_songs/fallo.mp3");
     audio.play();
     setTimeout(()=>{
-        derrota();
+    derrota();
     }, 50);
+    reintentar.show();
 }
 
 //Funcion que determina si un numero es entero o flotante.
@@ -49,6 +75,7 @@ function entero(n) {
         return false;
 }
 
+//Funcion que despliega una corona.
 function victoria(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
@@ -64,6 +91,7 @@ function victoria(){
     ctx.fill();
 }
 
+//Funcion que dibuja una carita triste.
 function derrota(){
     ctx.beginPath();
         ctx.font = "bold 30px Verdana";
@@ -72,6 +100,7 @@ function derrota(){
     ctx.closePath();
 }
 
+//Funcion que dibuja la base del tablero antes de empezar.
 function basePian(){
     for(let i=0; i<4; i++)
     {
@@ -113,6 +142,7 @@ function dib_teclas(carril, y){
     }
 }
 
+//Funcion que dibuja la division del canvas.
 function tabla(){
     //Variables que determinan la posicion de las lineas.
     let primera = canvas.width/4;
@@ -133,6 +163,7 @@ function tabla(){
     ctx.stroke();
 }
 
+//Funcion que permite comprobar si presionaste la teclaa correcta, y reproduce el sonido.
 function jugar(carril, flotante){
     if(teclas_rand[num_audio]==carril){
         audio = new Audio(audios[num_audio]);
@@ -140,6 +171,8 @@ function jugar(carril, flotante){
         teclas_rand[num_audio]=flotante;
         vel+=0.03;
         num_audio++;
+        puntaje(num_audio,vel);
+        //Si llegas al final, cancela la animacion y ejecuta la victoria.
         if(num_audio == TECLAS[song_rand])
         {
             setTimeout(()=>{
@@ -148,56 +181,63 @@ function jugar(carril, flotante){
             }, 500);
         }
     }
+    //Si no presionaste la respuesta correcta, ejecuta fin.
     else
         fin(); 
 }
 
-//Variable que controla como van bajando las teclas, se incializa una tecla antes de la que es.
-let y = -tecla.alto;
-
 //Inicializacion de el juego.
-basePian();
-dib_teclas(teclas_rand, y);
-tabla();
+inicializar();
 
 //ANIMACIONES ----------------------------------------------------------------------------------
 let animacion;
-let vel = 0;
 function draw(){
+    console.log("Sigue animacion");
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     dib_teclas(teclas_rand, y);
     tabla();
     y+=1+vel; //VELOCIDAD
     if(fallo == false)
-        animacion = requestAnimationFrame(draw);
-    else
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    animacion = requestAnimationFrame(draw);    
 }
 
-//EVENTOS DEL TECLADO --------------------------------------------------------------------------
-$("#duracion").click(()=>{
-    $("#duracion").hide();
+//EVENTOS --------------------------------------------------------------------------
+
+//Evento de boton de reintentar. 
+reintentar.click(()=>{
+    console.log("boton reintentar");
+    reintentar.hide();
+    inicializar();
+    setTimeout(()=>{
+        draw();
+    }, 1000);
+});
+
+//Evento de boton de jugar.
+$("#empezar").click(()=>{
+    $("#empezar").hide();
     draw();
 
+    //Eventos del teclado.
     $("body").keydown((e)=>{
         if(fallo == false && (e.key == 'a' || e.key == 's' || e.key == 'd' || e.key == 'f'))
         {
             switch(e.key)
             {
                 case 'a':
-                jugar(0,0.0000000000000000000000000000000001);
+                    jugar(0,0.0000000000000000000000000000000001);
                 break;
 
                 case 's':
-                jugar(1,0.1);
+                    jugar(1,0.1);
                 break;
 
                 case 'd':
-                jugar(2,0.2);
+                    jugar(2,0.2);
                 break;
 
                 case 'f':
-                jugar(3,0.3);
+                    jugar(3,0.3);
                 break;
             }
         }
