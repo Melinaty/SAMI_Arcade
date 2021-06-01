26e195b9ed2b07f1c6eb206a1a63f0865cd41865
Mel <melinatarchundiat@gmail.com>
2021-06-01 01:49:12 -0500

    FIX animacion

--- dynamics/js/animacionIndex.js
+++ dynamics/js/animacionIndex.js
@@ -3,10 +3,16 @@ window.addEventListener('load', ()=>{
     let sami = document.getElementById("sami");
     let samiestatica = document.getElementById("muestraSami");
     let boton = document.getElementById("boton");
+    let maquina1 = document.getElementById("muestraizq");
+    let maquina2 = document.getElementById("muestrader");
+    let letras = document.getElementsByClassName("letras");
     setTimeout(()=>{
         muestra.style.display="block";
+        for(i=0; i<6; i++)
+        {
+            letras[i].classList.add("none");
+        }
         sami.classList.add("samibaja");
-
     },2000)
     setTimeout(()=>{
         samiestatica.style.display="block";
@@ -14,7 +20,13 @@ window.addEventListener('load', ()=>{
     setTimeout(()=>{
         samiestatica.classList.add("blur");
         muestra.classList.add("blur");
-        boton.style.display="block";
+        sami.style.display="none";
+        
     },5000)
+    setTimeout(()=>{
+        boton.style.display="block";
+        maquina1.style.display="block";
+        maquina2.style.display="block";
+    },5200)
     
 })
