bae1d85c91057dd9497a8a3c59fc3cd4cbfffe10
Maurici0ML <alanmauriciomoraleslopez@gmail.com>
2021-06-02 00:03:04 -0500

    FEAT: puntajes

--- dynamics/js/puntaje.js
+++ dynamics/js/puntaje.js
@@ -1,40 +1,38 @@
 $(document).ready(()=>{
-    //Función que nos da el valor de la cookie
-    function valCookie(nombreCookie,x) 
+    //Función que nos da el valor de la cookie}
+    let arregloUsuarios = new Array(); 
+    let arregloPuntajes = new Array();
+    function puntajes() 
     {
         let cookies = document.cookie;
         let arrCookies=cookies.split("; ");
-
+        
         //Ciclo que nos separa cada valor en el nombre, y en el valor de la cookie y lo va asignando a un arreglo.
         for(const valor of arrCookies)
         {
             //Separando el valor obtenido en 2, donde el indice 0 es el nombre de la cookie, y el 1 es el valor.
             const cookie = valor.split('=');
             //if que nos permite verificar si el nombre es el que buscamos.
-            if (cookie[0] === nombreCookie)
+            if (cookie[0] != "activo")
             {
-                if(x==1)
-                    return cookie[0];
-                else if(x == 2)
-                    return cookie[1];
+                arregloUsuarios.push(cookie[0]);
+                arregloPuntajes.push(cookie[1].substr(1));
             }
         }
-        return null;
     }
-    //cuando ya existe la cookie le cambia el color  de fondo al que ya tenía antes
-    function cambiarColor(){
-        let colorAct = valCookie("activo", 2);
-        if(valCookie(colorAct, 2) == 'b')
-        {
-            cookiePal = 'b';
-            $("body").css("backgroundColor", "#d6d6d6"); 
-        }
-        else if(valCookie(colorAct, 2) == 'n')
-        {
-            cookiePal='n';
-        $("body").css("backgroundColor", "#242222");
-        }
+    
+    puntajes();
+    console.log(arregloUsuarios);
+    console.log(arregloPuntajes);
+    let tablau;
+    let tablapunt;
+    for(let i=0;i<arregloUsuarios.length; i++)
+    {
+        tablau = $('<p>'+arregloUsuarios[i]+'</p>');
+        tablau.css("color", "black");
+        $("#usuarioP").append(tablau);
+        tablapunt = $('<p>'+arregloPuntajes[i]+'</p>');
+        tablapunt.css("color", "black");
+        $("#puntajeP").append(tablapunt);
     }
-
-    cambiarColor();
 });
